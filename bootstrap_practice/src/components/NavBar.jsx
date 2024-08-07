307ba224834e839f65e8fbc9327d72e59752fd04
Jjaclyn Alday <jaalday123@gmail.com>
2024-08-07 15:09:29 -0400

    burger menu

--- bootstrap_practice/src/components/NavBar.jsx
+++ bootstrap_practice/src/components/NavBar.jsx
@@ -1,12 +1,34 @@
 
 import { Link } from "react-router-dom";
+import { useState } from "react";
+
+
+// const menu_btn = document.querySelector(".hamburger");
+// const mobile_menu = document.querySelector(".nav-mobile");
+// menu_btn.addEventListener("click", () => {
+//   menu_btn.classList.toggle("is-active");
+//     mobile_menu.classList.toggle("is-active");
+// });
+
+
 
 
 const NavBar = () => {
+
+
+const [isActive, setActive] = useState("false");
+const handleToggle = () => {
+    setActive(!isActive);
+};
+
+
+
+
   return (
+    <>
     <nav className="nav">
         <div className="logo">
-            <Link to ="/"><img className="logo"src="https://static.vecteezy.com/system/resources/previews/036/443/250/original/neon-light-alphabet-letter-j-png.png" alt="logo" /></Link>
+            <Link to ="/"><img className="logo"src="" alt="logo" /></Link>
         </div>
       
        <div className="nav-text2">
@@ -16,10 +38,30 @@ const NavBar = () => {
       
           <Link className="nav-text" to="/contact">Contact</Link>
           <Link className="nav-text" to="/projects">Projects</Link>
+ 
           </div>
+          <button className={`hamburger ${isActive ? 'is-active' : ''}`}
+            onClick={handleToggle}
+            >
+    <div className="bar"></div>
+  </button>
+  <nav className={`nav-mobile ${isActive ? 'is-active' : ''}`}
+            >
+  {/* <Link className="nav-text" to="/about">About</Link> */}
+  <a href="/contact" className="nav-text">Contact</a>
+  <a href="/projects" className="nav-text">Projects</a>
+  <a href="/about" className="nav-text">About</a>
+      
+
+  </nav>
+          
    
-   
+  
     </nav>
+ 
+
+  
+  </>
   );
 }
 
